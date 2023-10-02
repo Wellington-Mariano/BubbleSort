c085ed62fbdb230985aba8a55b3d01f0c63ea89f
Wellington-Mariano <wellington.mariano97@hotmail.com>
2023-10-02 16:35:17 +0100

    Fixed bug, boolean set to true when swap is made

--- src/main/java/com/mycompany/myfirstbubblesort/MyFirstBubbleSort.java
+++ src/main/java/com/mycompany/myfirstbubblesort/MyFirstBubbleSort.java
@@ -14,39 +14,50 @@ public class MyFirstBubbleSort {
 
     public static void main(String[] args) {
         Scanner mySc = new Scanner(System.in);
+        int namesNumber=0;
+        int index = 0;
         
         System.out.println("How many students are you ordering:");
-        int namesNumber = mySc.nextInt(), index = 1;
-        String arr[] = new String[namesNumber]; //5
+        namesNumber = mySc.nextInt();
+        
+        String arr[] = new String[namesNumber]; //0 1 2 3 4
         
         do{
             System.out.println("Type name:");
-            arr[index-1] = mySc.next();
+            arr[index] = mySc.next(); // 1 2 3 4 5
             index++;
-        }while(index <= namesNumber);
+        }while(index < namesNumber); //index goes up to 4
             
         System.out.println("\n");
-        //do a for to print unordered loop
+       /* 
+        for (String arrEl : arr) {
+            System.out.println(arrEl);
+        }*/
+        
         bubbleSort(arr);
     }
     
     public static void bubbleSort (String[] arr){
         int n = arr.length; //5
         String temp;
+        
         int firstLetterFirstName, firstLetterSecondName;
         
         for(int i=0; i<n; i++){
             boolean swapped = false;
-            for(int j=0; j<n-1-i; j++){ //Bubble sort is sending the biggest number to the end
+            for(int j=0; j < n-1-i; j++){ //Bubble sort is sending the biggest number to the end
                 firstLetterFirstName = (int) arr[j].charAt(0);
                 firstLetterSecondName = (int) arr[j+1].charAt(0);
+                              
                     if(firstLetterFirstName > firstLetterSecondName){ //IF 60 > 30 - Swap
                         temp = arr[j+1];
                         arr[j+1] = arr[j];
                         arr[j] = temp;
+                        swapped = true;
                     }
                 }
-            if(!swapped)
+            
+           if(!swapped)
             {
                break;
             }
